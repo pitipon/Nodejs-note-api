c40b4a84e74662e1c2c91d6a5a53eb69a994783d
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-11-02 12:04:19 +0800

    config mongodb_ui on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 // CONFIG MONGOOSE
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {
     mongoose
