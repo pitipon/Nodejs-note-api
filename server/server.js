11380280929f80c37e477bdf61efa447ebc27f64
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-11-02 11:13:45 +0800

    add test for /todos/:id

--- server/server.js
+++ server/server.js
@@ -49,15 +49,7 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(404).send()
     })
-    // Valid id using isValid
-        // 404 - send back empty send
-
-    // findById
-        // success
-            // if todo - send it back
-            // if no todo - send back 404 with empty body
-        // error
-            // 400 - and send empty body back
+   
 })
 
 // START SERVER AT PORT 3000
