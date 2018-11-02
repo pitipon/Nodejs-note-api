da94a030952297350503809c3b253e2d2465c283
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-11-02 14:11:21 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 var express = require('express')
 var bodyParser = require('body-parser')
 var { mongoose } = require('./db/mongoose')
@@ -77,6 +78,33 @@ app.delete('/todos/:id', (req, res) => {
         res.send({todo})
     }).catch((e) => {
         res.status(400).send()
+    })
+})
+
+// PATCH
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id
+    var body = _.pick(req.body, ['text','completed'])
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+
+        res.send({todo})
+    }).catch((e) => {
+        res.status(404).send()
     })
 })
 
