137ce719f096f5389e384e4eff5499c1c101326f
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-11-12 11:06:44 +0800

    config

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,21 @@
 var env = process.env.NODE_ENV || 'development'
 console.log('env *******', env)
 
-if (env === 'development') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-    // var mongoPassword = process.env.MONGODBPASSWORD || ''
-    // process.env.MONGODB_URI = `mongodb+srv://mo:${mongoPassword}@qrrice-fmwoi.azure.mongodb.net/TodoApp?retryWrites=true`
-} else if (env === 'test') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+// if (env === 'development') {
+//     process.env.PORT = 3000
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+//     // var mongoPassword = process.env.MONGODBPASSWORD || ''
+//     // process.env.MONGODB_URI = `mongodb+srv://mo:${mongoPassword}@qrrice-fmwoi.azure.mongodb.net/TodoApp?retryWrites=true`
+// } else if (env === 'test') {
+//     process.env.PORT = 3000
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+}
+
+if (env ==='development' || env === 'test') {
+    var config = require('./config.json')
+    var envConfig = config[env]
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]
+    })
 }
